2dc734992af558acef2de173f82617b8768f5ac0
Toru Ogawa <Hakuyume@users.noreply.github.com>
2024-11-23 18:30:20 +0900

    export to sqlite

--- firefox-exporter/examples/export.rs
+++ firefox-exporter/examples/export.rs
@@ -1,20 +1,18 @@
-use chrono::{DateTime, Utc};
+use chrono::Utc;
 use clap::Parser;
 use futures::TryStreamExt;
-use serde::Serialize;
 use std::path::PathBuf;
 use std::pin;
 use std::time::Duration;
-use tokio::io::AsyncWriteExt;
 
 #[derive(Parser)]
 struct Args {
     #[clap(long, default_value = "default")]
     profile: String,
-    #[clap(long)]
-    out: PathBuf,
     #[clap(long, value_parser = humantime::parse_duration, default_value = "5s")]
     interval: Duration,
+    #[clap(long)]
+    database: PathBuf,
 }
 
 #[tokio::main]
@@ -24,25 +22,25 @@ async fn main() -> anyhow::Result<()> {
     let args = Args::parse();
 
     let mut exporter = firefox_exporter::FirefoxExporter::new(&args.profile).await?;
+    let pool = sqlx::SqlitePool::connect_with(
+        sqlx::sqlite::SqliteConnectOptions::new()
+            .create_if_missing(true)
+            .filename(&args.database),
+    )
+    .await?;
+
+    sqlx::query("CREATE TABLE IF NOT EXISTS saves(timestamp UNIQUE, value)")
+        .execute(&pool)
+        .await?;
+
     let mut saves = pin::pin!(exporter.watch(args.interval));
     while let Some(save) = saves.try_next().await? {
-        #[derive(Serialize)]
-        struct Line {
-            timestamp: DateTime<Utc>,
-            save: String,
-        }
-        let mut f = tokio::fs::File::options()
-            .append(true)
-            .create(true)
-            .open(&args.out)
+        sqlx::query("INSERT OR REPLACE INTO saves (timestamp, value) values (?, ?)")
+            .bind(Utc::now())
+            .bind(&save)
+            .execute(&pool)
             .await?;
-        f.write_all(&serde_json::to_vec(&Line {
-            timestamp: Utc::now(),
-            save,
-        })?)
-        .await?;
-        f.write_all(b"\n").await?;
-        f.flush().await?;
     }
+
     Ok(())
 }
