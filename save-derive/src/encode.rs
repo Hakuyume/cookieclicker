bd97ca34a0be6856182e97eaeb6e13d7b1a04ad5
Toru Ogawa <Hakuyume@users.noreply.github.com>
2024-11-25 22:05:37 +0900

    refactor

--- save-derive/src/encode.rs
+++ save-derive/src/encode.rs
@@ -1,3 +1,5 @@
+use std::iter;
+
 pub(super) fn derive(input: &super::Input) -> syn::Item {
     let impl_generics = input.impl_generics();
     let ty_generics = input.ty_generics();
@@ -18,20 +20,22 @@ pub(super) fn derive(input: &super::Input) -> syn::Item {
                     syn::parse_quote!(#as_: __format::EncodeAs<#ty>)
                 },
             ));
-            let blocks = fields.iter().enumerate().map(
-                |(i, super::FieldNamed { as_, ident, ty })| -> syn::Block {
-                    let as_ = as_.as_ref().unwrap_or(&as_default);
-                    let split: Option<syn::Stmt> = if let (Some(split), 0) = (split, i) {
-                        Some(syn::parse_quote!(__fmt::Display::fmt(&#split, f)?;))
-                    } else {
-                        None
-                    };
-                    syn::parse_quote!({
-                        #split
-                        <#as_ as __format::EncodeAs<#ty>>::encode_as(&self.#ident, f)?;
-                    })
-                },
-            );
+            let exprs =
+                fields
+                    .iter()
+                    .enumerate()
+                    .flat_map(|(i, super::FieldNamed { as_, ident, ty })| {
+                        let as_ = as_.as_ref().unwrap_or(&as_default);
+                        split
+                            .iter()
+                            .filter(move |_| i > 0)
+                            .map(|split| -> syn::Expr {
+                                syn::parse_quote!(__fmt::Display::fmt(&#split, f))
+                            })
+                            .chain(iter::once(syn::parse_quote!(
+                                <#as_ as __format::EncodeAs<#ty>>::encode_as(&self.#ident, f)
+                            )))
+                    });
             syn::parse_quote!(
                 const _: () = {
                     use crate::format as __format;
@@ -42,7 +46,7 @@ pub(super) fn derive(input: &super::Input) -> syn::Item {
                     #(#where_predicates,)*
                     {
                         fn encode(&self, f: &mut __fmt::Formatter<'_>) -> __fmt::Result {
-                            #(#blocks)*
+                            #(#exprs?;)*
                             Ok(())
                         }
                     }
